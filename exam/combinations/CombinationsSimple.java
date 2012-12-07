32567e16b63af59cdd53de605d824d47bdd5e180
kgalieva <katerina.galieva@gmail.com>
2012-12-08 02:07:44 +0400

    subsets

---
+++ exam/combinations/CombinationsSimple.java
@@ -0,0 +1,24 @@
+package exam.combinations;
+
+public class CombinationsSimple {
+
+	public static void print(int[] binaryArray) {
+		for (int i = 0; i < binaryArray.length; i++) {
+			System.out.printf("%d ", binaryArray[i]);
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		int n = 6;
+		int[] a = new int[n];
+		for (int j = 0; j < Math.pow(2, n); j++) {		
+			a[n - 1] = j;
+			for(int i = n - 1; i > 0; i--){
+				a[i - 1] = a[i]/2;
+				a[i] %= 2;
+			}
+			print(a);			
+		}		
+	}
+}
