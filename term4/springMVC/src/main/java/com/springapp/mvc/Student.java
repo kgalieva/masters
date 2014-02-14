ef4bd59b5c46bcc4df9f81304f4bad9349680177
Ekaterina Galieva <katerina.galieva@gmail.com>
2014-02-15 03:41:14 +0400

    spring MVC

---
+++ term4/springMVC/src/main/java/com/springapp/mvc/Student.java
@@ -0,0 +1,28 @@
+package com.springapp.mvc;
+
+public class Student {
+   private Integer age;
+   private String name;
+   private Integer id;
+
+   public void setAge(Integer age) {
+      this.age = age;
+   }
+   public Integer getAge() {
+      return age;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getName() {
+      return name;
+   }
+
+   public void setId(Integer id) {
+      this.id = id;
+   }
+   public Integer getId() {
+      return id;
+   }
+}
